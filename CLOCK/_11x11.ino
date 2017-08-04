0f3090dac4d373682dd10573b03bbd08884d866e
TobiSchneider <tobihimself@web.de>
2017-08-04 21:05:09 +0200

    Update _11x11.ino

--- CLOCK/_11x11.ino
+++ CLOCK/_11x11.ino
@@ -1,3 +1,5 @@
+#include <FS.h>
+#include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
@@ -22,7 +24,6 @@
 #define pause    500
 #define initTime 100
 //------------------------------------------------
-
 static const int phraseES[4]        = {0,0,1,10};
 static const int phraseIST[4]       = {0,3,5,10};
 static const int phraseWLAN[4]      = {0,7,10,10};
@@ -52,11 +53,11 @@ static const int hourACHT[4]        = {0,1,4,1};
 static const int hourZEHNs[4]       = {0,5,8,1};
 static const int hourSECHS[4]       = {0,1,5,0};
 static const int phraseUHR[4]       = {0,8,10,0};
-
 //------------------------------------------------
   int hours;
   int minutes;
   int i = 1;
+  int offset=242;
   int mytimemonth;
   int mytimeday;
   int mytimehr;
@@ -68,13 +69,17 @@ static const int phraseUHR[4]       = {0,8,10,0};
   char *hourTIME;
   char *minTIME;
   char *secTIME;
-  char auth[] = "7a7d8af34e37437cae1d203c74cb943e";
+  char auth[] = "";
   int trigger = 0;
   String Ntime;
   int color=WHITE;
   String regg[55];
   String theDate;
   String BORN="Fri, 14 Jul 2017 0:0:0 GMT";
+  boolean extended_1 = 1;
+  boolean extended_2 = 1;
+  //------------------------------------------------
+
 
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(11, 11, 5,
   NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
@@ -117,59 +122,61 @@ void initClock(){
 }
 
 void displayWords() {
-  for (int i = 0; i<12; i++) {
+  for (int i = 0; i<11; i++) {
     matrix.drawLine(0,i,10,i,0);
   }
   //Always on
-  matrix.drawLine(phraseES[1],phraseES[3],phraseES[2],phraseES[3],color);
-  matrix.drawLine(phraseIST[1],phraseIST[3],phraseIST[2],phraseIST[3],color);
+  if(extended_1){
+      matrix.drawLine(phraseES[1],phraseES[3],phraseES[2],phraseES[3],color);
+      matrix.drawLine(phraseIST[1],phraseIST[3],phraseIST[2],phraseIST[3],color);
+    }
     // define the dots
     if((minutes%10)==1 || (minutes%10)==6 ){
-      matrix.drawLine(0,11,0,11,color);
+      matrix.drawLine(0,10,0,10,color);
     }
     if((minutes%10)==2 || (minutes%10)==7 ){
-      matrix.drawLine(0,11,0,11,color);
-      matrix.drawLine(1,11,1,11,color);
+      matrix.drawLine(0,10,0,10,color);
+      matrix.drawLine(1,10,1,10,color);
     }
     if((minutes%10)==3 || (minutes%10)==8){
-      matrix.drawLine(0,11,0,11,color);
-      matrix.drawLine(1,11,1,11,color);
-      matrix.drawLine(2,11,2,11,color);
+      matrix.drawLine(0,10,0,10,color);
+      matrix.drawLine(1,10,1,10,color);
+      matrix.drawLine(2,10,2,10,color);
     }
     if((minutes%10)==4 || (minutes%10)==9){
-      matrix.drawLine(0,11,0,11,color);
-      matrix.drawLine(1,11,1,11,color);
-      matrix.drawLine(2,11,2,11,color);
-      matrix.drawLine(3,11,3,11,color);
+      matrix.drawLine(0,10,0,10,color);
+      matrix.drawLine(1,10,1,10,color);
+      matrix.drawLine(2,10,2,10,color);
+      matrix.drawLine(3,10,3,10,color);
     }
   //calculate minutes on the hour
-    if(minutes<30 || minutes>34){
+    if(minutes<5){
     matrix.drawLine(phraseUHR[1],phraseUHR[3],phraseUHR[2],phraseUHR[3],color);
-    matrix.drawLine(phraseMINUTEN[1],phraseMINUTEN[3],phraseMINUTEN[2],phraseMINUTEN[3],color);
     trigger=1;
     }
+    if(((minutes>5 && minutes<25) || minutes>35) && extended_2){
+    matrix.drawLine(phraseUHR[1],phraseUHR[3],phraseUHR[2],phraseUHR[3],color);
+    }
+    if(minutes<30 || minutes>34){
+      matrix.drawLine(phraseMINUTEN[1],phraseMINUTEN[3],phraseMINUTEN[2],phraseMINUTEN[3],color);
+    }
     if(minutes>4 && minutes<10){
-    matrix.drawLine(phraseMINUTEN[1],phraseMINUTEN[3],phraseMINUTEN[2],phraseMINUTEN[3],color);
     matrix.drawLine(hourFUENFm[1],hourFUENFm[3],hourFUENFm[2],hourFUENFm[3],color);
     matrix.drawLine(phraseNACH[1],phraseNACH[3],phraseNACH[2],phraseNACH[3],color);
     }
     if(minutes>9 && minutes<15){
-    matrix.drawLine(phraseMINUTEN[1],phraseMINUTEN[3],phraseMINUTEN[2],phraseMINUTEN[3],color);
     matrix.drawLine(hourZEHNm[1],hourZEHNm[3],hourZEHNm[2],hourZEHNm[3],color);
     matrix.drawLine(phraseNACH[1],phraseNACH[3],phraseNACH[2],phraseNACH[3],color);
     }
     if(minutes>14 && minutes<20){
-    matrix.drawLine(phraseMINUTEN[1],phraseMINUTEN[3],phraseMINUTEN[2],phraseMINUTEN[3],color);
     matrix.drawLine(phraseVIERTEL[1],phraseVIERTEL[3],phraseVIERTEL[2],phraseVIERTEL[3],color);
     matrix.drawLine(phraseNACH[1],phraseNACH[3],phraseNACH[2],phraseNACH[3],color);
     }
     if(minutes>19 && minutes<25){
-    matrix.drawLine(phraseMINUTEN[1],phraseMINUTEN[3],phraseMINUTEN[2],phraseMINUTEN[3],color);
     matrix.drawLine(hourZWANZIG[1],hourZWANZIG[3],hourZWANZIG[2],hourZWANZIG[3],color);
     matrix.drawLine(phraseNACH[1],phraseNACH[3],phraseNACH[2],phraseNACH[3],color);
     }
     if(minutes>24 && minutes<30){
-    matrix.drawLine(phraseMINUTEN[1],phraseMINUTEN[3],phraseMINUTEN[2],phraseMINUTEN[3],color);
     matrix.drawLine(hourFUENFm[1],hourFUENFm[3],hourFUENFm[2],hourFUENFm[3],color);
     matrix.drawLine(phraseVor[1],phraseVor[3],phraseVor[2],phraseVor[3],color);
     matrix.drawLine(phraseHALB[1],phraseHALB[3],phraseHALB[2],phraseHALB[3],color);
@@ -178,110 +185,105 @@ void displayWords() {
     matrix.drawLine(phraseHALB[1],phraseHALB[3],phraseHALB[2],phraseHALB[3],color);
     }
     if(minutes>34 && minutes<40){
-    matrix.drawLine(phraseMINUTEN[1],phraseMINUTEN[3],phraseMINUTEN[2],phraseMINUTEN[3],color);
     matrix.drawLine(hourFUENFm[1],hourFUENFm[3],hourFUENFm[2],hourFUENFm[3],color);
     matrix.drawLine(phraseNACH[1],phraseNACH[3],phraseNACH[2],phraseNACH[3],color);
     matrix.drawLine(phraseHALB[1],phraseHALB[3],phraseHALB[2],phraseHALB[3],color);
     }
     if(minutes>39 && minutes<45){
-    matrix.drawLine(phraseMINUTEN[1],phraseMINUTEN[3],phraseMINUTEN[2],phraseMINUTEN[3],color);
     matrix.drawLine(hourZWANZIG[1],hourZWANZIG[3],hourZWANZIG[2],hourZWANZIG[3],color);
     matrix.drawLine(phraseVor[1],phraseVor[3],phraseVor[2],phraseVor[3],color);
     }
     if(minutes>44 && minutes<50){
-    matrix.drawLine(phraseMINUTEN[1],phraseMINUTEN[3],phraseMINUTEN[2],phraseMINUTEN[3],color);
     matrix.drawLine(phraseVIERTEL[1],phraseVIERTEL[3],phraseVIERTEL[2],phraseVIERTEL[3],color);
     matrix.drawLine(phraseVor[1],phraseVor[3],phraseVor[2],phraseVor[3],color);
     }
     if(minutes>49 && minutes<55){
-    matrix.drawLine(phraseMINUTEN[1],phraseMINUTEN[3],phraseMINUTEN[2],phraseMINUTEN[3],color);
     matrix.drawLine(hourZEHNm[1],hourZEHNm[3],hourZEHNm[2],hourZEHNm[3],color);
     matrix.drawLine(phraseVor[1],phraseVor[3],phraseVor[2],phraseVor[3],color);
     }
     if(minutes>54 && minutes<60){
-    matrix.drawLine(phraseMINUTEN[1],phraseMINUTEN[3],phraseMINUTEN[2],phraseMINUTEN[3],color);
     matrix.drawLine(hourFUENFm[1],hourFUENFm[3],hourFUENFm[2],hourFUENFm[3],color);
     matrix.drawLine(phraseVor[1],phraseVor[3],phraseVor[2],phraseVor[3],color);
     }
   if(minutes<25){
     // Calculate hour
-    if(hours==1 || hours==13 ){
+    if((hours%12)==1){
       if(trigger==1){
         matrix.drawLine(hourEIN[1],hourEIN[3],hourEIN[2],hourEIN[3],color);
       }else{
         matrix.drawLine(hourEINS[1],hourEINS[3],hourEINS[2],hourEINS[3],color);
       }
     }
-    if(hours==2 || hours==14 ){
+    if((hours%12)==2){
      matrix.drawLine(hourZwei[1],hourZwei[3],hourZwei[2],hourZwei[3],color);
     }
-    if(hours==3 || hours==15 ){
+    if((hours%12)==3){
     matrix.drawLine(hourDREI[1],hourDREI[3],hourDREI[2],hourDREI[3],color);
     }
-    if(hours==4 || hours==16 ){
+    if((hours%12)==4){
      matrix.drawLine(hourVIER[1],hourVIER[3],hourVIER[2],hourVIER[3],color);
     }
-    if(hours==5 || hours==17 ){
+    if((hours%12)==5){
      matrix.drawLine(hourFUENFs[1],hourFUENFs[3],hourFUENFs[2],hourFUENFs[3],color);
     }
-    if(hours==6 || hours==18 ){
+    if((hours%12)==6){
      matrix.drawLine(hourSECHS[1],hourSECHS[3],hourSECHS[2],hourSECHS[3],color);
     }
-    if(hours==7 || hours==19 ){
+    if((hours%12)==7){
      matrix.drawLine(hourSIEBEN[1],hourSIEBEN[3],hourSIEBEN[2],hourSIEBEN[3],color);
     }
-    if(hours==8 || hours==20 ){
+    if((hours%12)==8){
      matrix.drawLine(hourACHT[1],hourACHT[3],hourACHT[2],hourACHT[3],color);
     }
-    if(hours==9 || hours==21 ){
+    if((hours%12)==9){
      matrix.drawLine(hourNEUN[1],hourNEUN[3],hourNEUN[2],hourNEUN[3],color);
     }
-    if(hours==10 || hours==22 ){
+    if((hours%12)==10){
      matrix.drawLine(hourZEHNs[1],hourZEHNs[3],hourZEHNs[2],hourZEHNs[3],color);
     }
-    if(hours==11 || hours==23 ){
+    if((hours%12)==11){
      matrix.drawLine(hourELF[1],hourELF[3],hourELF[2],hourELF[3],color);
     }
-    if(hours==12 || hours==0 ){
+    if((hours%12)==0){
     matrix.drawLine(hourZWOELF[1],hourZWOELF[3],hourZWOELF[2],hourZWOELF[3],color);
     }
   }
   if(minutes>24){
     // Calculate hour
-    if(hours==25 || hours==13 ){
+    if((hours%12)==1){
      matrix.drawLine(hourZwei[1],hourZwei[3],hourZwei[2],hourZwei[3],color);
     }
-    if(hours==2 || hours==14 ){
+    if((hours%12)==2){
     matrix.drawLine(hourDREI[1],hourDREI[3],hourDREI[2],hourDREI[3],color);
     }
-    if(hours==3 || hours==15 ){
+    if((hours%12)==3){
      matrix.drawLine(hourVIER[1],hourVIER[3],hourVIER[2],hourVIER[3],color);
     }
-    if(hours==4 || hours==16 ){
+    if((hours%12)==4){
      matrix.drawLine(hourFUENFs[1],hourFUENFs[3],hourFUENFs[2],hourFUENFs[3],color);
     }
-    if(hours==5 || hours==17 ){
+    if((hours%12)==5){
      matrix.drawLine(hourSECHS[1],hourSECHS[3],hourSECHS[2],hourSECHS[3],color);
     }
-    if(hours==6 || hours==18 ){
+    if((hours%12)==6){
      matrix.drawLine(hourSIEBEN[1],hourSIEBEN[3],hourSIEBEN[2],hourSIEBEN[3],color);
     }
-    if(hours==7 || hours==19 ){
+    if((hours%12)==7){
      matrix.drawLine(hourACHT[1],hourACHT[3],hourACHT[2],hourACHT[3],color);
     }
-    if(hours==8 || hours==20 ){
+    if((hours%12)==8){
      matrix.drawLine(hourNEUN[1],hourNEUN[3],hourNEUN[2],hourNEUN[3],color);
     }
-    if(hours==9 || hours==21 ){
+    if((hours%12)==9){
      matrix.drawLine(hourZEHNs[1],hourZEHNs[3],hourZEHNs[2],hourZEHNs[3],color);
     }
-    if(hours==10 || hours==22 ){
+    if((hours%12)==10){
      matrix.drawLine(hourELF[1],hourELF[3],hourELF[2],hourELF[3],color);
     }
-    if(hours==11 || hours==23 ){
+    if((hours%12)==11){
     matrix.drawLine(hourZWOELF[1],hourZWOELF[3],hourZWOELF[2],hourZWOELF[3],color);
     }
-    if(hours==12 || hours==24 ){
+    if((hours%12)==0){
     matrix.drawLine(hourEINS[1],hourEINS[3],hourEINS[2],hourEINS[3],color);
     }
   }
@@ -332,6 +334,10 @@ void initWifi() {
   delay(1000);
 // -----------------------------------------------------------------------
   WiFiManager wifiManager;
+  wifiManager.setAPStaticIPConfig(IPAddress(10,0,1,1), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
+  wifiManager.setSTAStaticIPConfig(IPAddress(192,168,2,104), IPAddress(192,168,2,1), IPAddress(255,255,255,0));
+
   wifiManager.autoConnect("Wordclock", "Batman123");      //Log In zum Wifimanager
   Serial.println();
   Serial.print("Connecting to ");
@@ -366,7 +372,7 @@ String hourMIN() {
       g = strtok (NULL, ":");
     }
   }
-  hours=atoi( hourTIME)+2;
+  hours=atoi( hourTIME)+offset;
   minutes=atoi( minTIME);
   return TIME;
 }
@@ -380,6 +386,7 @@ void MyServer() {
 
   if (client.available()){
     String req = client.readStringUntil('\r');
+    Serial.println(req );
     if (req.indexOf("favicon") == -1){
       Serial.println(req);
       Serial.println(Ntime);
@@ -388,47 +395,84 @@ void MyServer() {
       char copy[30];
       req.toCharArray(copy, 30);
       // Match the request
-      if (req.indexOf("/color/") != -1){
-        switch (copy[11]){
-          case 'w':
-                Serial.println("set Color: WHITE" );
-                color=WHITE;
-                break;
-          case 'y':
-                color=YELLOW;
-                break;
-          case 'g':
-                color=GREEN;
-                break;
-          case 'b':
-                color=BLUE;
-                break;
-          case 'r':
-                color=RED;
-                break;
-          case 'm':
-                color=MAGENTA;
-                break;
+      Serial.println(copy );
+      switch (copy[5]){
           case 'c':
-                color=CYAN;
+             switch (copy[11]){
+                case 'w':
+                      color=WHITE;
+                      break;
+                case 'y':
+                      color=YELLOW;
+                      break;
+                case 'g':
+                      color=GREEN;
+                      break;
+                case 'b':
+                      color=BLUE;
+                      break;
+                case 'r':
+                      color=RED;
+                      break;
+                case 'm':
+                      color=MAGENTA;
+                      break;
+                case 'c':
+                      color=CYAN;
+                    break;
+                }
+                   break;
+
+          case 'b':{
+                char numbers[3];
+                numbers[0] = copy[16];
+                numbers[1] = copy[17];
+                numbers[2] = copy[18];
+                numbers[3] = '\0';
+                int numbersC = atoi(numbers);
+                matrix.setBrightness (numbersC);
+                Serial.println("set brightness to: " );
+                Serial.println(numbersC);
                 break;
-        }
-      }
-      else if (req.indexOf("/brightness/") != -1){
-      char numbers[3];
-      numbers[0] = copy[16];
-      numbers[1] = copy[17];
-      numbers[2] = copy[18];
-      numbers[3] = '\0';
-      int numbersC = atoi(numbers);
-      matrix.setBrightness (numbersC);
-      Serial.println("set brightness: " );
-      Serial.println(numbersC);
-      }
-      else {
-        Serial.println("invalid request");
-        return;
-      }
+            }
+
+          case 't':
+             switch (copy[11]){
+                case 'p':
+                      offset++;
+                      Serial.println(hours);
+                      Serial.println(offset );
+                      break;
+                case 'm':
+                      offset--;
+                      Serial.println(hours);
+                      Serial.println(offset );
+                      break;
+                case 'r':
+                      offset=242;
+                      Serial.println(hours);
+                      Serial.println(offset );
+                      break;
+                  }
+                   break;
+
+          case 'm':
+             switch (copy[11]){
+                case 'e':
+                      Serial.println(extended_1 );
+                      extended_1 = !extended_1;
+                      Serial.println(extended_1 );
+                      break;
+                case 'u':
+                      Serial.println(extended_2 );
+                      extended_2 = !extended_2;
+                      Serial.println(extended_2 );
+                      break;
+                }
+                   break;
+            }
+
+
       client.flush();
       // response
       String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\nCOLOR is now ";
@@ -441,7 +485,6 @@ void MyServer() {
 }
 
 void setup() {
-    // Debug console
   Serial.begin(9600);
   matrix.begin();
   matrix.setBrightness (128);
